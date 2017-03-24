feb2304e7b6d86adbb13dcceed1a20021e263c57
Padraic O'Neill <pconeill@gmail.com>
2017-03-24 11:57:08 -0600

    add class exists test

--- test/key_generator_test.rb
+++ test/key_generator_test.rb
@@ -4,5 +4,10 @@ require 'pry'
 require './lib/key_generator.rb'
 
 class KeyGeneratorTest < Minitest::Test
+  def test_class_exists
+    key_gen = KeyGenerator.new
+
+    refute key_gen.nil?
+  end
 
 end
