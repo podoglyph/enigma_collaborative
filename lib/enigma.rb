db42b188c29e5e25949f1ce1a4f51e9b56c27ea1
Padraic O'Neill <pconeill@gmail.com>
2017-03-25 14:39:53 -0600

    starter frame for enigma

---
+++ lib/enigma.rb
@@ -0,0 +1,24 @@
+require 'pry'
+
+class Enigma
+
+  def initialize
+    @message = message
+    @key = key
+    @date = nil
+  end
+
+  def encrypt(message, key = nil, date = nil)
+
+
+  end
+
+  def decrypt
+
+  end
+
+  def crack
+
+  end
+
+end
