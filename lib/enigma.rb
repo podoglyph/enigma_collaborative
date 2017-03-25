8116de7e5db397ec4191271bbb28c80630e5859d
Padraic O'Neill <pconeill@gmail.com>
2017-03-25 16:49:57 -0600

    add #assign_rotation

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -11,6 +11,15 @@ class Enigma
   def key_generator
     key = Random.new
     key = key.rand(10000..99999)
+    key = key.to_s
+  end
+
+  def assign_rotation
+    a_rotation = key_generator[0] + key_generator[1]
+    b_rotation = key_generator[1] + key_generator[2]
+    c_rotation = key_generator[2] + key_generator[3]
+    d_rotation = key_generator[3] + key_generator[4]
+
   end
 
   def get_date_as_string
@@ -20,10 +29,10 @@ class Enigma
   end
 
   def generate_offsets
-    a = get_date_as_string[-1]
-    b = get_date_as_string[-2]
-    c = get_date_as_string[-3]
-    d = get_date_as_string[-4]
+    a_offsets = get_date_as_string[-4].to_i
+    b_offsets = get_date_as_string[-3].to_i
+    c_offsets = get_date_as_string[-2].to_i
+    d_offsets = get_date_as_string[-1].to_i
   end
 
   def offsetter
