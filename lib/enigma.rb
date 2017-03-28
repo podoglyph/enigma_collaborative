f60519d1c437d3c88e3465267f398b3f6915ea7a
Padraic O'Neill <pconeill@gmail.com>
2017-03-27 19:38:14 -0600

    moved Enigma#key_gen to KeyGen#key_gen, refactored initialize to automatically call KeyGen

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,31 +1,23 @@
 require 'pry'
+require 'pry-state'
+require './lib/key_gen.rb'
 
 class Enigma
-  attr_reader :key, :character_map
+  attr_reader :character_map, :new_key
 
   def initialize
+    @new_key = KeyGen.new.key
     @message = ""
-    @key = key
-    @date = nil
-    @a = 0
-    @b = 0
-    @c = 0
-    @d = 0
+    @a, @b, @c, @d, @date = nil
     #@TODO Add space, comma, period
     @character_map = ('a'..'z').to_a
   end
 
-  def key_generator
-    key = Random.new
-    key = key.rand(10000..99999)
-    @key = key.to_s
-  end
-
   def assign_rotation
-    @a = (key[0] + key[1]).to_i
-    @b = (key[1] + key[2]).to_i
-    @c = (key[2] + key[3]).to_i
-    @d = (key[3] + key[4]).to_i
+    @a = (new_key[0] + new_key[1]).to_i
+    @b = (new_key[1] + new_key[2]).to_i
+    @c = (new_key[2] + new_key[3]).to_i
+    @d = (new_key[3] + new_key[4]).to_i
 
   end
 
@@ -40,35 +32,6 @@ class Enigma
     @b += get_date_as_string[-3].to_i
     @c += get_date_as_string[-2].to_i
     @d += get_date_as_string[-1].to_i
-  end
-
-  def encrypt
-    # 1. Take in the message.
-    # 2. Extract four characters at a time.
-    # 3. Add encryption values to each character.
-    # 4. Output it all to another file.
-    # 5. Continue until it's finished.
-    message = "This is a secret message ..end.."
-
-    message.each_char do |x|
-      i = 0
-    end
-    new_message = ""
-    i = 0
-    j = 1
-    k = 2
-    l = 3
-
-    until message.length == new_message.length
-      new_message << character_map.index(message[i])
-      new_message << character_map.index(message[j])
-      new_message << character_map.index(message[k])
-      new_message << character_map.index(message[l])
-      i += 4
-      j += 4
-      k += 4
-      l += 4
-    end
   end
 
   def decrypt
