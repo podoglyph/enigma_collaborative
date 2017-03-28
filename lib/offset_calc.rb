c0deefe30b686ed2fc880e6e30b743603d62dab6
Padraic O'Neill <pconeill@gmail.com>
2017-03-27 21:07:37 -0600

    moved offset calculator methods to offset_calc.rb

--- lib/offset_calc.rb
+++ lib/offset_calc.rb
@@ -6,5 +6,25 @@ class OffsetCalc
   def initialize
 
   end
-  
+
+  def assign_rotation
+    i = 0
+    j = 1
+    @rotation_values.map do |x|
+      #binding.pry
+      x = (new_key[i] + new_key[j]).to_i
+      @rotation_values[i] = x
+      #binding.pry
+      i += 1
+      j += 1
+    end
+  end
+
+  def add_offsets_to_rotation
+    @a += get_date_as_string[-4].to_i
+    @b += get_date_as_string[-3].to_i
+    @c += get_date_as_string[-2].to_i
+    @d += get_date_as_string[-1].to_i
+  end
+
 end
