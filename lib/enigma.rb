2a025f5e60ebdaa79f41f6cb0ed8cef3228d1fd7
Padraic O'Neill <pconeill@gmail.com>
2017-03-27 23:25:32 -0600

    moved methods over to OffsetCalc.rb

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -4,22 +4,15 @@ require './lib/key_gen.rb'
 require './lib/offset_calc.rb'
 
 class Enigma
-  attr_reader :character_map, :new_key, :rotation_values
+  attr_reader :character_map
 
   def initialize
-    @new_key = KeyGen.new.key
     @message = ""
-    @date = nil
-    @rotation_values = Array.new(4, nil)
     #@TODO Add space, comma, period
     @character_map = ('a'..'z').to_a
   end
 
-  def get_date_as_string
-    offset = Time.now.strftime("%d%m%y")
-    squared_offset = offset.to_i ** 2
-    squared_offset.to_s
-  end
+  
 
 
   def decrypt
