d521dc9b68fb4e35319c9fdcbf30482135600f36
adamgunther1 <adamgunther1@gmail.com>
2017-03-29 22:22:51 -0600

    begins cracked pseudocode

---
+++ lib/cracked.rb
@@ -0,0 +1,11 @@
+Run through every five digit combination.
+
+Plugin every combination as key and assessing whether the value of the last seven digits is ..end..
+
+When we determine what the correct key is
+
+
+
+until "..end.." == found_message
+
+  
