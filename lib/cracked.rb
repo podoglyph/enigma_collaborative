82724ef2223b883ed9d84a5407b5b02058f00f1a
adamgunther1 <adamgunther1@gmail.com>
2017-03-30 02:26:48 -0600

    cracked is functional

--- lib/cracked.rb
+++ lib/cracked.rb
@@ -51,8 +51,47 @@ class Cracked
 end
 
 
+  def unrotate_map
+    i = 0
+    rotated_map.each do |rotation|
+      unrotated = rotation.rotate(-first_offset_value_for_last_encrypted_character)#(-offset_values[i])
+      decrypted_chars << unrotated.first
+      i += 1
+      i = 0 if i == 4
+    end
+    decrypted_chars
+  end
+  def crack_map
+    end_of_message = output[-7..-1].chars
+    i = -1
+    until i < -7
+      end_of_message[i]
+      i -= 1
+    end
+    rotated_map.each do |rotation|
+    end
+  end
+
+  def offset_valuator
+    first_offset_value_for_last_encrypted_character = character_map.index(end_of_message[-1]) - character_map.index(".")
+    second_offset_value = rotated_map.index(end_of_message[-2]) - rotated_map.index(".")
+    third_offset_value = rotated_map.index(end_of_message[-3]) - rotated_map.index("d")
+    fourth_offset_value = rotated_map.index(end_of_message[-4]) - rotated_map.index("n")
+    reversed_rotated_map = rotated_map.reverse
+    
 
 
+    unencrypted_message.reverse #since going through backwards, need to reverse back after reversing map
+    
+  #   encryption_length = rotated_map.length
+  #   i = encryption_length  
+  #     until i < 0
+  #       rotated_map[i]
+
+  #     i -= 1
+  #     end
+  # end
+#rotation = index + offset
 
 puts generated_potential_key
 binding.pry
--- lib/enigma.rb
+++ lib/enigma.rb
@@ -4,19 +4,24 @@ require './lib/encryptor.rb'
 require './lib/decryptor.rb'
 
 class Enigma
-  attr_reader :my_message, :key, :offset, :rotated_map
+  attr_reader :my_message, :key, :offset, :rotated_map,
+
+              :character_map
 
   def initialize
     @my_message = my_message
     @offset = nil
     @rotated_map = nil
+
+    @character_map = (' '..'z').to_a
   end
 
   def encrypt(my_message, key = nil, date = nil)
     encryption = Encryptor.new(my_message)
     encryption.encryptor
     @key = encryption.offset_calc.key
-    @formatted_date = encryption.offset_calc.formatted_date
+    # @formatted_date = encryption.offset_calc.formatted_date
+    @formatted_date = encryption.offset_calc.format_date
     @the_date = encryption.offset_date
     @offset = encryption.offset_values
     @rotated_map = encryption.rotated_map
@@ -31,7 +36,29 @@ class Enigma
   end
 
   def crack(output, date = @formatted_date)
-
+    end_of_message = output[-7..-1].chars
+    crack_offset_values = []
+    cracked_chars = []
+    offset_value_for_last_encrypted_character = character_map.index(end_of_message[-1]) - character_map.index(".")
+    crack_offset_values << offset_value_for_last_encrypted_character
+    offset_value_for_second_last_encrypted_character = character_map.index(end_of_message[-2]) - character_map.index(".")
+    crack_offset_values << offset_value_for_second_last_encrypted_character
+    offset_value_for_third_last_encrypted_character = character_map.index(end_of_message[-3]) - character_map.index("d")
+    crack_offset_values << offset_value_for_third_last_encrypted_character
+    first_offset_value_for_last_encrypted_character = character_map.index(end_of_message[-4]) - character_map.index("n")
+    crack_offset_values << first_offset_value_for_last_encrypted_character
+    reversed_rotated_map = rotated_map.reverse
+    i = 0
+    reversed_rotated_map.each do |rotation|
+      unrotated = rotation.rotate(-crack_offset_values[i])
+      cracked_chars << unrotated.first
+      i += 1
+      i = 0 if i == 4
+    end    
+    cracked_message = cracked_chars.reverse.join
+    p cracked_message
+    
+    
   end
 
 
